1ad4f15122136d1381e10549dac2ed243ac0e885
Roman Zajavkowy <jakis@mail.pl>
2023-04-01 21:26:36 +0200

    zmiany w pliku Cat

--- Cat.java
+++ Cat.java
@@ -2,5 +2,9 @@ public class Cat {
 
     public static void main(String[] args) {
         System.out.println("Hello cat!");
+        System.out.println("Hello cat!");
+        System.out.println("Hello cat!");
+        System.out.println("Hello cat!");
+        System.out.println("Hello cat!");
     }
 }
