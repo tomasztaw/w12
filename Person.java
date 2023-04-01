5ff0f1162472f6f98fb94e33a5f1a56ee7f48cbe
tomasz <tomasz.wegrzyn.wro@gmail.com>
2023-04-01 11:38:28 +0200

    feature-commit-1. Rebase on main branch:)

--- Person.java
+++ Person.java
@@ -2,13 +2,19 @@ import java.math.BigDecimal;
 
 public class Person {
 
+    public static final String HEIGHT = "100";
+
     private String name;
     private BigDecimal salary;
+    private String pesel;
+
 
     public Integer age() {
         return 42;
     }
 
-    public static final String HEIGHT = "100";
+    public String whatIsYourHobby() {
+        return "pizza";
+    }
 
 }
