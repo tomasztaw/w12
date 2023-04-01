6113f1b0e26bcd78e9fdde60aaaab5c15d718d32
tomasz <tomasz.wegrzyn.wro@gmail.com>
2023-04-01 21:09:56 +0200

    PersonTwo update 3

--- PersonTwo.java
+++ PersonTwo.java
@@ -3,5 +3,6 @@ public class PersonTwo {
     public static void main(String[] args) {
         System.out.println("Hello 1");
         System.out.println("Hello 2");
+        System.out.println("Hello 3");
     }
 }
